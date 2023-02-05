f3297a31f4eac1fb2bd21cc27316fcb9404c0cc4
Artem <artem-orlov92@bk.ru>
2023-02-05 13:29:24 +0300

    Initial Commit

---
+++ Millionaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Millionaire
+//
+//  Created by Артем Орлов on 05.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

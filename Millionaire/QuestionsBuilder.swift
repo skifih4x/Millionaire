d78cd617ba18b2df914c47b5f2c1a4eb2cbb6b9a
Dmitrii642 <dmit.iakovlev@gmail.com>
2023-02-05 18:08:13 +0300

    Added game questions

---
+++ Millionaire/QuestionsBuilder.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionsBuilder.swift
+//  Millionaire
+//
+//  Created by Dmitrii Iakovlev on 05.02.2023.
+//
+
+import Foundation
+struct Question {
+    let question: String
+    let answer1: String
+    let answer2: String
+    let answer3: String
+    let answer4: String
+    let rightAnswer: Int
+    let cash: Int
+}

67a47c000214983f9b363b3c67b48cee76e76a9c
emirmalika <emirmalika0712@gmail.com>
2023-02-09 16:32:25 +0600

    цвета кнопок

--- Millionaire/QuestionViewController.swift
+++ Millionaire/QuestionViewController.swift
@@ -12,33 +12,32 @@ class QuestionViewController: UIViewController {
     
     var player: AVAudioPlayer!
     var audioPlayerTimer = Timer()
-    var bgChangedTimer = Timer()
 
     let allQuestions = AllQuestions()
     var currentQuestion = 0
     var isCorrect: Bool?
-    var correctAnswer: Int?
+    var answerTag: Int?
     
     
     @IBOutlet weak var questionLbl: UILabel!
     @IBOutlet var answersLbl: [UILabel]!
+    @IBOutlet var answersBtn: [UIButton]!
     
     @IBAction func answerBtnPresesd(_ sender: UIButton) {
         if sender.tag == allQuestions.questions[currentQuestion].rightAnswer {
             isCorrect = true
-            correctAnswer = sender.tag
-            bgChangedTimer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(self.stopSound), userInfo: nil, repeats: false)
         } else {
             isCorrect = false
         }
-        playSound(name: "AnswerAccepted")
+        answerTag = sender.tag
+        answerChecking(name: "AnswerAccepted")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         getQuestion(index: currentQuestion)
-        playSound(name: "TimerSound")
+        answerChecking(name: "TimerSound")
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -64,12 +63,12 @@ class QuestionViewController: UIViewController {
         answersLbl[3].text = allQuestions.questions[index].answers[3]
     }
     
-    func playSound(name: String) {
+    func answerChecking(name: String) {
         if name == "AnswerAccepted" {
             let url = Bundle.main.url(forResource: name, withExtension: "mp3")
             player = try! AVAudioPlayer(contentsOf: url!)
             player.play()
-            audioPlayerTimer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(self.stopSound), userInfo: nil, repeats: false)
+            audioPlayerTimer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(self.checkStopped), userInfo: nil, repeats: false)
         } else {
             let url = Bundle.main.url(forResource: name, withExtension: "mp3")
             player = try! AVAudioPlayer(contentsOf: url!)
@@ -77,13 +76,14 @@ class QuestionViewController: UIViewController {
         }
     }
     
-    @objc func stopSound() {
+    @objc func checkStopped() {
         player.stop()
         if isCorrect == true {
-            playSound(name: "CorrectAnswer")
-
+            answersBtn[answerTag! - 1].setBackgroundImage(UIImage(named: "RectangleGreen"), for: .normal)
+            answerChecking(name: "CorrectAnswer")
         } else {
-            playSound(name: "WrongAnswer")
+            answersBtn[answerTag! - 1].setBackgroundImage(UIImage(named: "RectangleRed"), for: .normal)
+            answerChecking(name: "WrongAnswer")
         }
     }
 
